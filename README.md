9c3a5727ed05324ce133452b935b860b4fe92e00
maflbr <maflbr@dsb.dk>
2023-07-19 15:25:50 +0200

    Completed step 1-8

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # jetpack-compose-basic-layouts-codelab
+
+https://developer.android.com/codelabs/jetpack-compose-layouts?continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fjetpack-compose-for-android-developers-1%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fjetpack-compose-layouts#8
---
+++ app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -0,0 +1,307 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.codelab.basiclayouts
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.codelab.basiclayouts.ui.theme.MySootheTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { MySootheApp() }
+    }
+}
+
+// Step: Search bar - Modifiers
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier
+) {
+    var fieldValue by rememberSaveable { mutableStateOf("") }
+
+    TextField(
+        value = fieldValue,
+        onValueChange = { fieldValue = it },
+        leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = null) },
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = MaterialTheme.colors.surface
+        ),
+        placeholder = { Text(text = stringResource(R.string.placeholder_search)) },
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 56.dp)
+    )
+}
+
+// Step: Align your body - Alignment
+@Composable
+fun AlignYourBodyElement(
+    @DrawableRes imageRes: Int,
+    @StringRes textRes: Int,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(imageRes),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(88.dp)
+                .clip(CircleShape),
+        )
+
+        Text(
+            text = stringResource(textRes),
+            style = MaterialTheme.typography.h3,
+            modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp)
+        )
+    }
+}
+
+// Step: Favorite collection card - Material Surface
+@Composable
+fun FavoriteCollectionCard(
+    modifier: Modifier = Modifier,
+    @DrawableRes imageRes: Int,
+    @StringRes textRes: Int,
+) {
+    Surface(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.small
+    ) {
+        Row(
+            modifier = Modifier.size(width = 192.dp, height = 56.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(imageRes),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(56.dp)
+            )
+            Text(
+                text = stringResource(textRes),
+                style = MaterialTheme.typography.h3,
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+            )
+        }
+    }
+}
+
+// Step: Align your body row - Arrangements
+@Composable
+fun AlignYourBodyRow(
+    modifier: Modifier = Modifier,
+    items: List<DrawableStringPair>,
+) {
+    LazyRow(
+        modifier = modifier,
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(items) { item ->
+            AlignYourBodyElement(imageRes = item.drawable, textRes = item.text)
+        }
+    }
+}
+
+// Step: Favorite collections grid - LazyGrid
+@Composable
+fun FavoriteCollectionsGrid(
+    modifier: Modifier = Modifier,
+    items: List<DrawableStringPair>
+) {
+    LazyHorizontalGrid(
+        modifier = modifier.height(120.dp),
+        rows = GridCells.Fixed(2),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        items(items) { item ->
+            FavoriteCollectionCard(imageRes = item.drawable, textRes = item.text)
+        }
+    }
+}
+
+// Step: Home section - Slot APIs
+@Composable
+fun HomeSection(
+    modifier: Modifier = Modifier
+) {
+    // Implement composable here
+}
+
+// Step: Home screen - Scrolling
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    // Implement composable here
+}
+
+// Step: Bottom navigation - Material
+@Composable
+private fun SootheBottomNavigation(modifier: Modifier = Modifier) {
+    // Implement composable here
+}
+
+// Step: MySoothe App - Scaffold
+@Composable
+fun MySootheApp() {
+    // Implement composable here
+}
+
+private val alignYourBodyData = listOf(
+    R.drawable.ab1_inversions to R.string.ab1_inversions,
+    R.drawable.ab2_quick_yoga to R.string.ab2_quick_yoga,
+    R.drawable.ab3_stretching to R.string.ab3_stretching,
+    R.drawable.ab4_tabata to R.string.ab4_tabata,
+    R.drawable.ab5_hiit to R.string.ab5_hiit,
+    R.drawable.ab6_pre_natal_yoga to R.string.ab6_pre_natal_yoga
+).map { DrawableStringPair(it.first, it.second) }
+
+private val favoriteCollectionsData = listOf(
+    R.drawable.fc1_short_mantras to R.string.fc1_short_mantras,
+    R.drawable.fc2_nature_meditations to R.string.fc2_nature_meditations,
+    R.drawable.fc3_stress_and_anxiety to R.string.fc3_stress_and_anxiety,
+    R.drawable.fc4_self_massage to R.string.fc4_self_massage,
+    R.drawable.fc5_overwhelmed to R.string.fc5_overwhelmed,
+    R.drawable.fc6_nightly_wind_down to R.string.fc6_nightly_wind_down
+).map { DrawableStringPair(it.first, it.second) }
+
+data class DrawableStringPair(
+    @DrawableRes val drawable: Int,
+    @StringRes val text: Int
+)
+
+@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
+@Composable
+fun SearchBarPreview() {
+    MySootheTheme {
+        SearchBar(Modifier.padding(8.dp))
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
+@Composable
+fun AlignYourBodyElementPreview() {
+    MySootheTheme {
+        AlignYourBodyElement(
+            modifier = Modifier.padding(8.dp),
+            imageRes = R.drawable.ab1_inversions,
+            textRes = R.string.ab1_inversions
+        )
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
+@Composable
+fun FavoriteCollectionCardPreview() {
+    MySootheTheme {
+        FavoriteCollectionCard(
+            modifier = Modifier.padding(8.dp),
+            imageRes = R.drawable.fc2_nature_meditations,
+            textRes = R.string.fc2_nature_meditations
+        )
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
+@Composable
+fun AlignYourBodyRowPreview() {
+    MySootheTheme {
+        AlignYourBodyRow(
+            items = alignYourBodyData
+        )
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
+@Composable
+fun FavoriteCollectionsGridPreview() {
+    MySootheTheme { FavoriteCollectionsGrid(items = favoriteCollectionsData) }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
+@Composable
+fun HomeSectionPreview() {
+    MySootheTheme { HomeSection() }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
+@Composable
+fun ScreenContentPreview() {
+    MySootheTheme { HomeScreen() }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
+@Composable
+fun BottomNavigationPreview() {
+    MySootheTheme { SootheBottomNavigation(Modifier.padding(top = 24.dp)) }
+}
+
+@Preview(widthDp = 360, heightDp = 640)
+@Composable
+fun MySoothePreview() {
+    MySootheApp()
+}
